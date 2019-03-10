e26e83c06381db22d45111ea24b735c43706074f
Simon Arruti <simonarruti.dev@gmail.com>
2019-03-10 23:21:31 +0100

    single @ reference support OK

--- src/components/UserReferenceInput.vue
+++ src/components/UserReferenceInput.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="user_reference_input_component">
     <label>User Reference</label><br>
-    <textarea v-model="input_text" rows="20" cols="80"></textarea>
+    <textarea v-model="input_text" rows="20" cols="80" ref="text_element"></textarea>
+    <div v-html="input_text"></div>
     <ul class="users_list" v-show="show_list">
-      <li class="single_user" v-for="user of users" :key="user.id">{{`@${user.nick_name} (${user.first_name + " " + user.last_name})`}}</li>
+      <li class="single_user" v-for="user of users" v-show="!user.hidden" :key="user.id">
+        <a href="#" class="single_user_link" @click="addUserToReference(user.nick_name)">{{`@${user.nick_name} (${user.first_name + " " + user.last_name})`}}</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -15,19 +18,37 @@
     data () {
       return {
         input_text: '',
-        show_list: false
+        show_list: false,
+        users: users
       }
     },
-    computed: {
-      users () {
-        return users
+    methods: {
+      getInputCursorPosition () {
+        return this.$refs.text_element.selectionStart
+      },
+      searchList (stringToSearch) {
+        for (const user of users) {
+          if (user.first_name.indexOf(stringToSearch) > -1 || user.last_name.indexOf(stringToSearch) > -1 || user.nick_name.indexOf(stringToSearch) > -1) {
+            user.hidden = false
+          } else {
+            user.hidden = true
+          }
+        }
+      },
+      addUserToReference (userNickname) {
+        // const cursorPosition = this.getInputCursorPosition()
+        const stringToReplace = this.input_text.substring(this.input_text.indexOf('@') + 1)
+        const stringReady = stringToReplace.replace(/.*/, userNickname)
+        this.input_text = this.input_text.replace(/(@).*/, '$1' + stringReady + ' ')
+        this.$refs.text_element.focus()
       }
     },
     watch: {
       input_text () {
-        if (this.input_text.indexOf('@') > -1) {
-          console.log(this.input_text)
+        const atIndex = this.input_text.indexOf('@')
+        if (atIndex > -1) {
           this.show_list = true
+          this.searchList(this.input_text.substring(atIndex + 1))
         } else {
           this.show_list = false
         }
--- src/users.js
+++ src/users.js
@@ -3,18 +3,21 @@ export default [
     id: 1,
     first_name: 'Jean',
     last_name: 'Delatour',
-    nick_name: 'jd'
+    nick_name: 'jd',
+    hidden: false
   },
   {
     id: 2,
     first_name: 'Antoine',
     last_name: 'Lucsko',
-    nick_name: 'al'
+    nick_name: 'al',
+    hidden: false
   },
   {
     id: 3,
     first_name: 'Thierry',
     last_name: 'Tranchina',
-    nick_name: 'tt'
+    nick_name: 'tt',
+    hidden: false
   }
 ]
