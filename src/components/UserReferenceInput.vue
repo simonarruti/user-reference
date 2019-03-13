a7aa4b780a19307f8d98de8bb85b452f061ab648
sar <simon.arruti@1000mercis.com>
2019-03-13 14:25:43 +0100

    multi @ support OK

--- src/components/UserReferenceInput.vue
+++ src/components/UserReferenceInput.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="user_reference_input_component">
-    <label>User Reference</label><br>
-    <textarea v-model="input_text" rows="20" cols="80" ref="text_element" @input="inputText($event)"></textarea>
-    <div v-html="input_text"></div>
+    <h1>User Reference</h1><br>
+    <textarea v-model="input_text" rows="5" cols="50" ref="text_element" placeholder="Text..." @input="inputText($event)"></textarea>
+    <!--<div v-html="input_text"></div>-->
     <ul class="users_list" v-show="show_list">
+      <li><h3>Utilisateurs</h3></li>
       <li class="single_user" v-for="user of users" v-show="!user.hidden" :key="user.id">
         <a href="#" class="single_user_link" @click="addUserToReference(user)">{{`@${user.nick_name} (${user.first_name + " " + user.last_name})`}}</a>
       </li>
     </ul>
-    {{references}}
+    <div>
+      <h3>Références:</h3>
+      {{references}}
+    </div>
   </div>
 </template>
 
@@ -21,16 +25,24 @@
         input_text: '',
         show_list: false,
         users: users,
-        references: []
+        references: [],
+        current_search: ''
       }
     },
     methods: {
       getInputCursorPosition () {
         return this.$refs.text_element.selectionStart
       },
+      toggleSearchOff () {
+        this.show_list = false
+        this.current_search = ''
+
+        this.users.map(element => element.hidden = false)
+      },
       searchList (stringToSearch) {
+        console.log('SEARCH', stringToSearch)
         for (const user of users) {
-          if (user.first_name.indexOf(stringToSearch) > -1 || user.last_name.indexOf(stringToSearch) > -1 || user.nick_name.indexOf(stringToSearch) > -1) {
+          if (user.first_name.toLowerCase().includes(stringToSearch) || user.last_name.toLowerCase().includes(stringToSearch) || user.nick_name.toLowerCase().includes(stringToSearch)) {
             user.hidden = false
           } else {
             user.hidden = true
@@ -42,20 +54,43 @@
         if (event.data === '@') {
           const cursorPosition = this.getInputCursorPosition()
           console.log(cursorPosition)
+          this.show_list = true
+
+          return
+        }
+
+        if (this.show_list) {
+          if (event.data) {
+            this.current_search += event.data
+          }
+          this.searchList(this.current_search)
+
+          if (event.data === ' ') {
+            this.toggleSearchOff()
+          }
         }
       },
       addUserToReference (user) {
         // const cursorPosition = this.getInputCursorPosition()
-        const stringToReplace = this.input_text.substring(this.input_text.indexOf('@') + 1)
-        const stringReady = stringToReplace.replace(/.*/, user.nick_name)
-        this.input_text = this.input_text.replace(/(@).*/, '$1' + stringReady + ' ')
+        const stringToReplace = this.input_text.substring(0, this.input_text.lastIndexOf('@') + 1)
+        const stringReady = stringToReplace + user.nick_name + ' '
+        console.log('READY', stringToReplace)
+        this.input_text = stringReady
+
         this.$refs.text_element.focus()
 
         this.references.push({
           id: this.references.length + 1,
-          user_ref: stringReady,
-          user_fullname: user.first_name + ' ' + user.last_name
+          user_ref: user.nick_name,
+          user_fullname: user.first_name + user.last_name
         })
+
+        this.toggleSearchOff()
+      }
+    },
+    watch: {
+      current_search () {
+        console.log(this.current_search)
       }
     }
     // watch: {
@@ -75,8 +110,14 @@
 <style lang="scss" scoped>
   .user_reference_input_component {
     .users_list {
-      position: absolute;
-      top: 0;
+      margin: 0;
+      padding: 0;
+      /*position: absolute;*/
+      /*top: 0;*/
+
+      .single_user {
+        list-style-type: none;
+      }
     }
   }
 </style>
